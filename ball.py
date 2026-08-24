134a55ffd937d47961f1f010d5eb4b1f6ce142f7
anish77 <annycvasniuc@yahoo.com>
2026-08-24 15:03:37 +0200

    add the bounce logic

--- ball.py
+++ ball.py
@@ -6,6 +6,13 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1 #Change the direction
