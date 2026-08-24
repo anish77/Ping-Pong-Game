944aba4cb365fde609450d21ab720e94cf3fde7c
anish77 <annycvasniuc@yahoo.com>
2026-08-24 16:12:48 +0200

    add the bounce logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -13,6 +14,7 @@ screen.tracer(0)  #Non aggiornare automaticamente lo schermo.
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball((0,0))
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -23,7 +25,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1) #to see the ball is going slowly
+    time.sleep(ball.move_speed) #to see the ball is going slowly
     screen.update()
     ball.move()
 
@@ -35,9 +37,11 @@ while game_is_on:
         ball.bounce_x()
     #Detect R paddle miss the ball
     elif ball.xcor() > 350:
+        scoreboard.l_point()
         ball.reset_position()
     # Detect L paddle miss the ball
     elif ball.xcor() < -350:
+        scoreboard.r_point()
         ball.reset_position()
 
 screen.exitonclick()
